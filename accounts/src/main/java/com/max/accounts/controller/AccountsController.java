120aa1a850872dc2b1dce9e7ae74dc5e11c66400
Voroshilov Maksim <voroshilovmax90@gmail.com>
2025-02-18 23:59:27 +0100

    init

---
+++ accounts/src/main/java/com/max/accounts/controller/AccountsController.java
@@ -0,0 +1,13 @@
+package com.max.accounts.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("sayHello")
+    public String sayHello() {
+        return "Hello world";
+    }
+}
