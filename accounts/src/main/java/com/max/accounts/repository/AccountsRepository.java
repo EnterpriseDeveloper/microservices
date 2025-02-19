d2adf898eee56544a257ed1e55c8cada227681a2
Voroshilov Maksim <voroshilovmax90@gmail.com>
2025-02-19 19:57:59 +0100

    fix: added Dto to projects

---
+++ accounts/src/main/java/com/max/accounts/repository/AccountsRepository.java
@@ -0,0 +1,20 @@
+package com.max.accounts.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.max.accounts.entity.Accounts;
+
+import jakarta.transaction.Transactional;
+
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+    Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
+
+}
